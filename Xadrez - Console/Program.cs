6c5fe3ddaed804964719e7e3a79dd037bdd452c3
Victor Gabriel de Oliveira <gabvictoroli@gmail.com>
2020-03-28 22:13:52 -0300

    Projeto Iniciado

---
+++ Xadrez - Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Xadrez___Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
